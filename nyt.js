91c928621c1244c96478fb1d93f48b6f80524895
Cody <chunt46511@gmail.com>
2019-08-14 17:42:04 -0400

    Due Thursday Morning

---
+++ nyt.js
@@ -0,0 +1,39 @@
+const baseURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json'; //1
+const key = 'S0iwMsrXcgsXcoHQY6IEPYMcGX7NmMMU'; //2
+let url; //3
+
+//Search Form
+const searchTerm = document.querySelector('.search');
+const startDate = document.querySelector('.start-date');
+const endDate = document.querySelector('.end-date');
+const searchForm = document.querySelector('form');
+const submitBtn = document.querySelector('.submit');
+
+//RESULTS NAVIGATION
+const nextBtn = document.querySelector('.next');
+const previousBtn = document.querySelector('.prev');
+const nav = document.querySelector('nav');
+
+//RESULTS SECTION
+const section = document.querySelector('section');
+
+nav.style.display = 'none';
+let pageNumber = 0;
+let displayNav = false;
+
+searchForm.addEventListener('submit', fetchResults);
+nextBtn.addEventListener('click', nextPage);
+previousBtn.addEventListener('click', previousPage);
+
+function fetchResults(e){
+    e.preventDefault();
+    url = baseURL + '?api-key=' + key + '&page=' + pageNumber + '&q=' + searchTerm.nodeValue;
+}
+
+function nextPage(){
+    console.log("Next button clicked");
+}
+
+function previousPage(){
+    console.log("Next button clicked");
+}
